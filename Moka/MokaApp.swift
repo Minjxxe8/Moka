4a278fbe6c8999230ec6860cdfb16f142100b70d
rlena <lena.ricard@ynov.com>
2025-10-18 10:58:19 +0200

    Initial Xcode Project

---
+++ Moka/MokaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MokaApp.swift
+//  Moka
+//
+//  Created by Ricard Léna on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MokaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MokaTests/MokaTests.swift
@@ -0,0 +1,16 @@
+//
+//  MokaTests.swift
+//  MokaTests
+//
+//  Created by Ricard Léna on 18/10/2025.
+//
+
+import Testing
+
+struct MokaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
